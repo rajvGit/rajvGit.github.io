6ad1af247cdb534917ce868761a1619236b109b2
rajvGit <71583002+rajvGit@users.noreply.github.com>
2022-01-28 18:12:46 +1300

    Add files via upload

---
+++ _next/static/i3Ft1tkFeEVRjGnknD6fv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b10551bc5d107618.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/blog/[blogId]":["static/chunks/pages/blog/[blogId]-78a39c16d2f4fe5d.js"],sortedPages:["/","/_app","/_error","/blog/[blogId]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
